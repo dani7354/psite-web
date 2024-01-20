56fa9209a1f96203ea979cd16ab24a204dd71a31
Daniel <d@stuhrs.dk>
2024-01-20 08:51:10 +0100

    Fix copy char

--- psite-web/app/View/footer.php
+++ psite-web/app/View/footer.php
@@ -5,7 +5,7 @@
 </main>
     <footer class="footer mt-auto py-3 bg-dark">
       <div class="container">
-        <span class="text-light">&copy <?php echo date('Y') . " " . FULL_NAME; ?> </span>
+        <span class="text-light">&copy; <?php echo date('Y') . " " . FULL_NAME; ?> </span>
       </div>
     </footer>
   </body>
